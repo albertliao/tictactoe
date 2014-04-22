60a4b3aee0c9ada08f07c16ca45c7f8ae8dbe027
Albert Liao <al_liao@yahoo.com>
2014-04-21 18:08:19 -0700

    updating project

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function calculateOutcome(r,c) {
 
 	//check row for win
 	var matrixScore = 0;
+	winMatrix = [[0,0,0],[0,0,0],[0,0,0]];
 	for (var i=0; i<3; i++) {
 		matrixScore += gameMatrix[r][i];
+		winMatrix[r][i] = 1;
 	}
-	console.log("row score:"+matrixScore);
+	//console.log("row score:"+matrixScore);
 
 	if (matrixScore == 3) {
 		return "X";
@@ -16,10 +18,12 @@ function calculateOutcome(r,c) {
 
 	//checking col for win
 	matrixScore = 0;
+	winMatrix = [[0,0,0],[0,0,0],[0,0,0]];
 	for (var i=0; i<3; i++) {
 		matrixScore += gameMatrix[i][c];
+		winMatrix[i][c] = 1;
 	}
-	console.log("col score:"+matrixScore);
+	//console.log("col score:"+matrixScore);
 
 	if (matrixScore == 3) {
 		return "X";
@@ -31,7 +35,8 @@ function calculateOutcome(r,c) {
 	//check for diagonals TL
 	matrixScore = 0;
 	matrixScore = gameMatrix[0][0]+gameMatrix[1][1]+gameMatrix[2][2];
-	console.log("diagTL score:"+matrixScore);
+	winMatrix = [[1,0,0],[0,1,0],[0,0,1]];
+	//console.log("diagTL score:"+matrixScore);
 
 	if (matrixScore == 3) {
 		return "X";
@@ -43,7 +48,8 @@ function calculateOutcome(r,c) {
 	//check for diagonals BL
 	matrixScore = 0;
 	matrixScore = gameMatrix[2][0]+gameMatrix[1][1]+gameMatrix[0][2];
-	console.log("diagBL score:"+matrixScore);
+	winMatrix = [[0,0,1],[0,1,0],[1,0,0]];
+	//console.log("diagBL score:"+matrixScore);
 
 	if (matrixScore == 3) {
 		return "X";
@@ -54,8 +60,9 @@ function calculateOutcome(r,c) {
 
 
 	//check for tie
+	winMatrix = [[0,0,0],[0,0,0],[0,0,0]];
 	if (movesTaken == MAX_TURNS) {
-		console.log("Game Over: Tied");
+		//console.log("Game Over: Tied");
 		return "Tied";
 	}
 	else {
@@ -66,15 +73,38 @@ function calculateOutcome(r,c) {
 
 function TicTacController($scope)
 {
-	console.log($scope);
+	//console.log($scope);
 	$scope.rows = [["", "", ""], ["","",""], ["","",""]];  //clear board
-	$scope.outcome = ""
+	$scope.outcome = "T3";
+	$scope.gameWinner = "";
+
+	$scope.showWinningCell = function(r,c) {
+		return (winMatrix[r][c] == 1 && $scope.gameWinner != "");
+	}
+
+	$scope.isCellFull = function(r,c) {
+		return ($scope.rows[r][c] != "" && !$scope.isGameOver() );
+	}
+
+	$scope.isGameOver = function() {
+		//console.log("Game Over:"+$scope.gameWinner);
+		return ($scope.gameWinner != "");
+	}
+
+	$scope.resetBoard = function() {
+		$scope.rows = [["", "", ""], ["","",""], ["","",""]];
+		$scope.outcome = "T3"
+		gameMatrix = [[0,0,0],[0,0,0],[0,0,0]];
+		$scope.gameWinner = "";
+		movesTaken = 0;
+		$scope.footerStyle = { "display":"none" };
+	}
 
 	$scope.playerMoved = function(r,c) {
-		console.log("makemove")
-		console.log("row:"+r+" col:"+c );
+		//console.log("makemove")
+		//console.log("row:"+r+" col:"+c );
 		
-		if (window.gameWinner == "" && $scope.rows[r][c] == "") {
+		if ($scope.gameWinner == "" && $scope.rows[r][c] == "") {
 			if (window.playerXTurn) {
 				$scope.rows[r][c] = 'X';
 				gameMatrix[r][c] = 1;
@@ -89,13 +119,14 @@ function TicTacController($scope)
 			}
 
 			//returns game outcome or "" if not done
-			window.gameWinner = calculateOutcome(r,c);
+			$scope.gameWinner = calculateOutcome(r,c);
 
-			if(window.gameWinner != "") {
-				if(window.gameWinner == "Tied")
+			if($scope.gameWinner != "") {
+				$scope.footerStyle = { "display":"block" }; //adjust style to block;
+				if($scope.gameWinner == "Tied")
 					$scope.outcome = "Draw!";
 				else
-					$scope.outcome = "Player "+gameWinner+" Wins!";
+					$scope.outcome = "Player "+$scope.gameWinner+" Wins!";
 			}
 				
 		}
@@ -103,28 +134,14 @@ function TicTacController($scope)
 	};
 }
 
-function resetBoard() {}
-function gameEnd() {
-	console.log("Game Over:"+gameWinner+" has won.");
-}
-function gameStart() {
 
-	//while game not over
-		//first player turn  (can use one player function, toggle global var)
-		//if game over -> end  
-		//else second player turn
-		//if game over -> end
-
-}
 
 window.onload = function() {
-	console.log("script start");
-
-	gameStart();
+	//console.log("script start");
 }
 
+var winMatrix = [[0,0,0],[0,0,0],[0,0,0]];
 var gameMatrix = [[0,0,0],[0,0,0],[0,0,0]];
-var gameWinner = "";
 var playerXTurn = true;
 var MAX_TURNS = 9;
 var movesTaken = 0;
